d21471961b42af86dc8eeaa32caec5825093f268
kmj-mkes <mkes.kmj@gmail.com>
2024-06-27 03:21:24 +0900

    Add files via upload

---
+++ java_04_mine/MemberArray_re.java
@@ -0,0 +1,45 @@
+package java_04;
+
+import java.util.Scanner;
+
+public class MemberArray_re {
+
+	private static Scanner sc = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		/**
+		 * [ 요구사항 ]
+		 * 
+		 *  1. 다섯명의 회원명을 입력받아 String 배열을 생성하시오.
+		 *  
+		 */
+
+		String[] names = memberInsert();
+		memberSelect(names);
+	}
+
+	// 다섯명의 회원명을 입력받아 String 배열을 생성할 method
+	public static String[] memberInsert() {
+
+		String[] names = new String[5];
+
+		for (int i=0; i<5; i++) {
+			System.out.print("회원 이름을 입력해 주세요 : ");
+			names[i] = sc.next();
+		}
+
+		System.out.println("---------------------------");
+		System.out.println("회원 정보 입력 완료 !!");
+		System.out.println("---------------------------");
+
+		return names;
+	}
+
+	// 생성된 회원정보를 출력하는 method
+	public static void memberSelect(String[] args) {
+		for (int i=0; i<args.length; i++) {
+			System.out.println("[ 회원 " + (i + 1) + " ] " + args[i]);
+		}
+	}
+
+}
